1c82f1fc8cfc482c1beb3f9476c4372c3f32f7e9
7yl4r <murray.tylar@gmail.com>
2019-11-16 09:57:07 -0500

    + another example ccf calc

--- plo7y/plotters/ccf_test.py
+++ plo7y/plotters/ccf_test.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 import matplotlib.pyplot as plt
 import sys
 
-from plo7y.plotters.ccf import plotCCF
 from plo7y._tests import get_test_output_path
 
 
@@ -21,17 +20,35 @@ def get_sample_data():
     plt.plot(x, y2)
     # plt.show()
     plt.savefig(get_test_output_path(__file__, "sample_data"))
-
+    plt.clf()
     dta = pd.DataFrame(y1)  # [x, y1, y2]  # TODO dataframe?
     exog = pd.DataFrame(y2)
     return dta, exog
 
 
-class Test_tsne(TestCase):
+class Test_ccf(TestCase):
+    def tearDown(self):
+        plt.clf()
+
     def test_ccf_plot_on_sample_data(self):
+        from plo7y.plotters.ccf import plotCCF
+
         dta, exog = get_sample_data()
         plotCCF(
             dta, exog, saveFigPath=get_test_output_path(
+                __file__, sys._getframe().f_code.co_name
+            )
+        )
+
+
+class Test_cross_correlation(TestCase):
+    def test_cross_correlation_on_sample_data(self):
+        from plo7y.plotters.cross_correlation import plot
+
+        dta, exog = get_sample_data()
+        plot(
+            # dta, exog,
+            saveFigPath=get_test_output_path(
                 __file__, sys._getframe().f_code.co_name
             )
         )
