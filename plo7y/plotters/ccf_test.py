3ea20064b91fac38afd212b41c16aee9e5dac570
7yl4r <murray.tylar@gmail.com>
2019-11-17 07:23:42 -0500

    + noisy sines test to scipy ccf

--- plo7y/plotters/ccf_test.py
+++ plo7y/plotters/ccf_test.py
@@ -88,12 +88,23 @@ class Test_cross_correlation(TestCase):
 
 
 class Test_ccf_scipy(TestCase):
-    def test_ccf_scipy_on_sample_data(self):
+    def test_ccf_scipy_on_noisy_binary(self):
         from plo7y.plotters.ccf_scipy import plot
 
         y1, y2 = _get_testdata_binary_and_noise()
         plot(
             y1, y2,
+            saveFigPath=get_test_output_path(
+                __file__, sys._getframe().f_code.co_name
+            )
+        )
+
+    def test_ccf_scipy_on_phased_sines(self):
+        from plo7y.plotters.ccf_scipy import plot
+
+        x, y1, y2 = _get_testdata_noisy_sines()
+        plot(
+            y1.squeeze().to_numpy(), y2.squeeze().to_numpy(),
             saveFigPath=get_test_output_path(
                 __file__, sys._getframe().f_code.co_name
             )
